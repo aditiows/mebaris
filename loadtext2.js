222ddda80b19f4943daa60090b8cc60e5525d8fb
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-17 00:54:52 +0800

    Create loadtext2.js

---
+++ loadtext2.js
@@ -0,0 +1,37 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
+<script>
+var host = "broker.hivemq.com";
+var port = "1883";
+var clientID = "58d3444cc5ed4bd0a2bc01acf7b28e81";
+
+
+// Create a client instance
+client = new Paho.MQTT.Client(host, Number(port), clientID);
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and save a message to a txt.
+  client.subscribe("mebaris01/nurusallam/");
+  
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
+</script>
