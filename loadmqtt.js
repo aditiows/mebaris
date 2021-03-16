69726a8ff3771f8a1f87190d2e0b9c0d94f604a1
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-17 01:08:23 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js"> type="text/javascript"</script>
 <script>
 var host = "broker.hivemq.com";
 var port = "1883";
