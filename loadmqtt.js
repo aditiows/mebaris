6159b110a172639857c204109a184bb55d03f095
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-24 00:32:22 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -1,26 +1,39 @@
-script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js"
-
-var host = "broker.hivemq.com";
-var port = "8000";
-var clientID = "58d3444cc5ed4bd0a2bc01acf7b28e81";
-
-
 // Create a client instance
-client = new Paho.MQTT.Client(host, Number(port), clientID);
-
+client = new Paho.MQTT.Client("broker.hivemq.com", Number(8083), "mebariswoi");
+client.startTrace();
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
 // connect the client
-client.connect({onSuccess:onConnect});
+client.connect({onSuccess:onConnect,
+                useSSL: true});
+console.log("attempting to connect...")
 
 
 // called when the client connects
 function onConnect() {
-  // Once a connection has been made, make a subscription and save a message to a txt.
-  client.subscribe("mebaris01/nurusallam/");
-  
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("/mebaris01/nurusallam/suhu");
+  message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "/mebaris01/nurusallam/suhu";
+  //client.send(message);
+//console.log(client.getTraceLog());
+
+  //client.getTraceLog().forEach(function(line){
+  //  console.log('Trace: ' + line)
+  //});
+  //newMessage = new Paho.MQTT.Message("Sent using synonyms!");
+  //newMessage.topic = "/World";
+  client.publish(message)
+  client.publish("/mebaris01/nurusallam/suhu", "Hello from a better publish call!", 1, false)
+
+  topicMessage = new Paho.MQTT.Message("This is a message where the topic is set by setTopic");
+  topicMessage.topic = "/mebaris01/nurusallam/suhu";
+  client.publish(topicMessage)
+
+
 }
 
 // called when the client loses its connection
