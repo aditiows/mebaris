050e37ad3cda3b3151062803478f0e5a961ee12c
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-24 11:54:24 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -1,5 +1,5 @@
 // Create a client instance
-client = new Paho.MQTT.Client("broker.emqx.io", Number(8083), "mebariswoi");
+client = new Paho.MQTT.Client("broker.emqx.io", Number(8084), "mebariswoi");
 client.startTrace();
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
