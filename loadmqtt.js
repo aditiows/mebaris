b1c66e02a8df1d90ddc8a883ffb461902d50cd4a
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-24 19:32:18 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -8,17 +8,17 @@ client.onMessageArrived = onMessageArrived;
 // connect the client
 client.connect({onSuccess:onConnect,
                 useSSL: true});
-console.log("mencoba untuk terkoneksi dengan " + host +" "+ port);
+console.log("mencoba untuk terkoneksi...");
 
 
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("Terkoneksi");
-  client.subscribe("/mebaris01/nurusallam/suhu");
-  //message = new Paho.MQTT.Message("Hello");
-  //message.destinationName = "/mebaris01/nurusallam/suhu";
-  //client.send(message);
+  client.subscribe("mebaris01/nurusallam/suhu");
+  message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "mebaris01/nurusallam/suhu";
+  client.send(message);
   //console.log(client.getTraceLog());
 
   //client.getTraceLog().forEach(function(line){
@@ -27,10 +27,10 @@ function onConnect() {
   //newMessage = new Paho.MQTT.Message("Sent using synonyms!");
   //newMessage.topic = "mebaris01/nurusallam/";
   //client.publish(message)
-  //client.publish("/mebaris01/nurusallam/suhu", "Hello from a better publish call!", 1, false)
+  //client.publish("mebaris01/nurusallam/suhu", "Hello from a better publish call!", 1, false)
 
   //topicMessage = new Paho.MQTT.Message("This is a message where the topic is set by setTopic");
- // topicMessage.topic = "/mebaris01/nurusallam/suhu";
+ // topicMessage.topic = "mebaris01/nurusallam/suhu";
   //client.publish(topicMessage)
 }
 
