5042b884907c084dc6c879085d5e9c3e4ef03816
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-28 14:17:48 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -52,7 +52,11 @@ function onConnectionLost(responseObject) {
 
 function onMessageArrived(message) {
     if (message.destinationName = "mebaris01/nurusallam") {
-    var data = JSON.parse(message.payloadString);
-    document.getElementById("lembap").innerHTML = (data.suhu+"°");
+    myData();
       }
+    }
+
+function myData() {
+    var data = JSON.parse(message.payloadString);
+    document.getElementById("suhu").innerHTML = (data.suhu+"°");
     }
