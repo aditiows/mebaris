88723a0bdbd40303df8bc052c96ea5ac6a2af81e
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-31 03:09:49 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -10,7 +10,6 @@ client.reconnect = true;
 client.connect({onSuccess:onConnect,
                 useSSL: true});
 console.log("mencoba untuk terkoneksi...");
-var message = new Paho.MQTT.Message('');
 
 // called when the client connects
 function onConnect() {
@@ -18,7 +17,7 @@ function onConnect() {
   console.log("Koneksi Berhasil");
   client.subscribe("mebaris_M234jkjDS4Jk23j");
   client.subscribe("mebaris_M234jkjDS4Jk23j/nurusallam");
-  //message = new Paho.MQTT.Message('{"suhu":40, "lembap":80}');
+  message = new Paho.MQTT.Message('{"suhu":40, "lembap":80}');
   message.destinationName = "mebaris_M234jkjDS4Jk23j/nurusallam";
   //message.qos = 1;
   //client.send(message);
