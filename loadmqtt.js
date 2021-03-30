b1b22c969f8e71fa294553eeb58acffbd8e20585
aditiows <35859976+aditiows@users.noreply.github.com>
2021-03-31 03:15:29 +0800

    Update loadmqtt.js

--- loadmqtt.js
+++ loadmqtt.js
@@ -17,7 +17,7 @@ function onConnect() {
   console.log("Koneksi Berhasil");
   client.subscribe("mebaris_M234jkjDS4Jk23j");
   client.subscribe("mebaris_M234jkjDS4Jk23j/nurusallam");
-  message = new Paho.MQTT.Message('{"suhu":40, "lembap":80}');
+  message = new Paho.MQTT.Message();
   message.destinationName = "mebaris_M234jkjDS4Jk23j/nurusallam";
   //message.qos = 1;
   //client.send(message);
@@ -43,18 +43,18 @@ function onConnectionLost(responseObject) {
 }
 
 // bila ada pesan masuk
-function onMessageArrived(message) {
+function onMessageArrived(msg) {
     if(message.destinationName = "mebaris_M234jkjDS4Jk23j/nurusallam") {
     tempe();
     hum();
     }
     else if(message.destinationName = "mebaris_M234jkjDS4Jk23j") {
-    console.log(message.destinationName+" "+message.payloadString);
+    console.log(message.destinationName+" "+msg.payloadString);
     }
 }
   
 var tempe = function myTemp() {
-              var mpls = message.payloadString;
+              var mpls = msg.payloadString;
               var myData = JSON.parse(mpls);
               var dataTemp = document.getElementById("suhu");
               dataTemp.innerHTML = (myData.suhu+"°");
@@ -62,7 +62,7 @@ var tempe = function myTemp() {
               }
 
 var hum = function myHumid() {
-             var mpls = message.payloadString;
+             var mpls = msg.payloadString;
              var myData = JSON.parse(mpls);
              var dataHum = document.getElementById("lembap");
              dataHum.innerHTML = (myData.lembap+"%");
